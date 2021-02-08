bd1046f535d0984d81720e6ea42a3be7b03a4dd6
andreinals <andreinamercia736@gmail.com>
2021-02-08 20:17:05 -0300

    segue_projeto

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,5 @@
-var frase = jQuery(".frase");
-console.log(frase);
+var frase = $(".frase").text();
+var numPalavras = frase.split(" ").length;
+
+var tamanhoFrase = $("#tamanho-frase");
+tamanhoFrase.text(numPalavras);
